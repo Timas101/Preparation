459182b1dc9d5bb0e48172d13233d71d64f5ea16
Timas101 <134627767+Timas101@users.noreply.github.com>
2025-02-27 14:34:11 +0300

    thurf

---
+++ KEGE/№17/14653/14653.py
@@ -0,0 +1,32 @@
+from math import sqrt
+
+massive = [int(x[:-1]) for x in open("14653.txt").readline()]
+
+del_17_list = []
+for element in massive:
+    if element % 17 == 0:
+        del_17_list.append(element)
+del_17_list.sort()
+summ_min_elements = del_17_list[0] + del_17_list[1]
+
+max_end_69_sqr = -10 ** 4
+for element in massive:
+    if str(element)[:-2] == '69':
+        max_end_69 = max(max_end_69, element)
+max_end_69_sqr = sqrt(max_end_69)
+
+result_list = []
+for i in range(len(massive) - 3):
+    count_three_len = 0
+    count_del_18 = 0
+    pr_elment = 1
+    for element in massive[i:i + 4]:
+        pr_elment *= element
+        if len(str(element)) == 3:
+            count_three_len += 1
+        if element % 18 == 0:
+            count_del_18 += 1
+    if count_three_len == 2 and count_del_18 == 1 and sum(massive[i:i + 4]) % summ_min_elements == 0 and pr_elment <= max_end_69_sqr:
+        result_list.append(sqrt(sum(massive[i:i + 4])))
+
+print(len(result_list), min(result_list))
