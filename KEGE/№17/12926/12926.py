3df2694096cc4b0a363ff8b1b48dbbb82251844f
Timas101 <134627767+Timas101@users.noreply.github.com>
2025-03-04 10:15:48 +0300

    sadasdasdasdasdasd

--- KEGE/№17/12926/12926.py
+++ KEGE/№17/12926/12926.py
@@ -1,5 +1,4 @@
-base_array = [int(x[:-2]) for x in open(r"C:\Users\timof\Documents\Preparation\KEGE\№17\12926\17_12926.txt")]
-
+base_array = [int(x[:-1]) for x in open(r"C:\Users\timof\Documents\Preparation\KEGE\№17\12926\17_12926.txt")]
 max_two_len_num = 0
 for element in base_array:
     if len(str(abs(element))) == 2:
@@ -9,12 +8,13 @@ max_sum_four = 0
 for i in range(len(base_array) - 4):
     sum_array = base_array[i:i + 4]
     same_end_check = True
-    end_int = sum_array[0] % 10
+    end_int = abs(sum_array[0]) % 10
     for element in sum_array:
-        if end_int != element % 10:
+        if end_int != abs(element) % 10:
             same_end_check = False
     if same_end_check == True:
         max_sum_four = max(sum(sum_array), max_sum_four)
+print(max_sum_four)
 
 count_five = 0
 sum_five_array = []
