b67ed322cadef0aca1e30c083f947be3e1dd3220
Timas101 <134627767+Timas101@users.noreply.github.com>
2025-03-09 13:36:25 +0300

    Create 20811.py

---
+++ KEGE/№20/20811.py
@@ -0,0 +1,12 @@
+def Check(num, count):
+    if count > 4 or num >= 51:
+        return count == 4
+    if count % 2 == 0:
+        return Check(num + 1, count + 1) or Check(num + 4, count + 1) or Check(num * 2, count + 1)
+    else:
+        return Check(num + 1, count + 1) and Check(num + 4, count + 1) and Check(num * 2, count + 1)
+    
+
+for s in range(1, 51):
+    if Check(s, 1):
+        print(s)
