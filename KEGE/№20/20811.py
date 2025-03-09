066ca9fb26624a68e1d1e780f63f6a4cff37fbde
Timas101 <134627767+Timas101@users.noreply.github.com>
2025-03-09 13:37:35 +0300

    Update 20811.py

--- KEGE/№20/20811.py
+++ KEGE/№20/20811.py
@@ -1,7 +1,7 @@
 def Check(num, count):
     if count > 4 or num >= 51:
         return count == 4
-    if count % 2 == 0:
+    if count % 2 != 0:
         return Check(num + 1, count + 1) or Check(num + 4, count + 1) or Check(num * 2, count + 1)
     else:
         return Check(num + 1, count + 1) and Check(num + 4, count + 1) and Check(num * 2, count + 1)
